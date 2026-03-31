e0652249a8d462fd5cee057a23cfac2d2091ce70
None-named <None_named_@outlook.com>
2026-03-31 20:25:03 +0800

    refactor:修复模型路径错误

--- model_test.py
+++ model_test.py
@@ -10,7 +10,7 @@ test_map_data = read_map.parse_map_file("maps/explore/map.txt")
 # 创建环境
 env = Env.SokobanEnv(test_map_data)
 
-loaded_model = PPO.load("Sokoban_Refined", env=env)
+loaded_model = PPO.load("out/explore/Sokoban_Refined.zip", env=env)
 
 # 运行示例
 obs, _ = env.reset()
