754f4d91710f00be0fd8e870602951e9905eafe1
None-named <None_named_@outlook.com>
2026-04-01 20:23:27 +0800

    feat:增加更多信息显示

--- model_test.py
+++ model_test.py
@@ -2,13 +2,15 @@ from stable_baselines3 import PPO
 import Env
 import utils
 import time
-import matplotlib.pyplot as plt
 
-category = utils.read_json_variable("states.json", "category")
-map_index = utils.read_json_variable("states.json", "map_index")
+# category = utils.read_json_variable("states.json", "category")
+category = "explore"
+# map_index = utils.read_json_variable("states.json", "map_index")
+map_index = 1
 
 map_path = "maps/" + category + "/map" + str(map_index) + ".txt"
 out_path = "out/" + category
+actions_name = {0: "UP", 1: "DOWN", 2: "LEFT", 3: "RIGHT"}
 
 # 定义地图（符号表示）
 test_map_data = utils.parse_map_file(map_path)
@@ -24,14 +26,17 @@ loaded_model = PPO.load(model, env=env)
 # 运行示例
 obs, _ = env.reset()
 
-steps = 0  # 记录步数
+steps = 1  # 记录步数
 start_time = time.time()  # 记录开始时间
+total_reward = 0
 
 while True:
     action, _ = loaded_model.predict(obs, deterministic=True)
     action = int(action)
     obs, reward, terminated, truncated, info = env.step(action)
-    if terminated:
+    total_reward += reward
+    print("Step Info:", "step:", steps, "action:", actions_name[action], "reward:", reward)
+    if terminated or truncated:
         obs, _ = env.reset()
         break
     env.render()
@@ -39,6 +44,13 @@ while True:
     steps += 1
 env.close()
 
+if reward > 0:
+    print("Successful")
+else:
+    print("Fail")
+
 end_time = time.time()  # 记录结束时间
 elapsed_time = end_time - start_time - steps * 0.25  # 计算耗时
 print("步数:", steps, "推测用时:", elapsed_time, "s")
+print("累计奖励:", total_reward)
+
