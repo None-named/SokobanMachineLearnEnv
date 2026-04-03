0910ea29c8b5a446a9dc482234df42d172010176
None-named <None_named_@outlook.com>
2026-04-03 18:14:33 +0800

    refactor:将测试ppo模型封装成一个函数

--- model_test.py
+++ model_test.py
@@ -3,54 +3,50 @@ import Env
 import utils
 import time
 
-# category = utils.read_json_variable("states.json", "category")
-category = "explore"
-# map_index = utils.read_json_variable("states.json", "map_index")
-map_index = 1
+def test_ppo_model(model: PPO, env: Env.SokobanEnv, render: bool) -> bool:
+    obs, _ = env.reset()
+    # 统计信息
+    total_reward = 0
+    total_steps = 0
+    start_time = time.time()
 
-map_path = "maps/" + category + "/map" + str(map_index) + ".txt"
-out_path = "out/" + category
-actions_name = {0: "UP", 1: "DOWN", 2: "LEFT", 3: "RIGHT"}
+    while True:
+        action, _ = model.predict(obs, deterministic=True)
+        action = int(action)
+        obs, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+        total_steps += 1
+        if render:
+            env.render()
+            time.sleep(0.25)
+            print("Step Info:", "step:", total_steps, "action:", actions_name[action], "reward:", reward)
+        if terminated or truncated:
+            obs, _ = env.reset()
+            break
+    end_time = time.time()
+    env.close()
+    if render:
+        elapsed_time = end_time - start_time - total_steps * 0.25
+        print("Total Reward:", total_reward)
+        print("Total Time:", elapsed_time)
+        print("Total Steps:", total_steps)
+    else:
+        elapsed_time = end_time - total_steps
+    return total_reward > 0
 
-# 定义地图（符号表示）
-test_map_data = utils.parse_map_file(map_path)
-print("地图:", map_path)
 
-# 创建环境
-env = Env.SokobanEnv(test_map_data)
+if "__main__" == __name__:
+    # category = utils.read_json_variable("states.json", "category")
+    category = "finish_goal"
+    # map_index = utils.read_json_variable("states.json", "map_index")
+    map_index = 2
 
-model = utils.get_latest_file_concise(out_path)
-print("模型:", model)
-loaded_model = PPO.load(model, env=env)
+    map_path = "maps/" + category + "/map" + str(map_index) + ".txt"
+    out_path = "out/" + category
+    actions_name = {0: "UP", 1: "DOWN", 2: "LEFT", 3: "RIGHT"}
 
-# 运行示例
-obs, _ = env.reset()
+    # 定义地图（符号表示）
+    test_map_data = utils.parse_map_file(map_path)
+    print("地图:", map_path)
 
-steps = 1  # 记录步数
-start_time = time.time()  # 记录开始时间
-total_reward = 0
-
-while True:
-    action, _ = loaded_model.predict(obs, deterministic=True)
-    action = int(action)
-    obs, reward, terminated, truncated, info = env.step(action)
-    total_reward += reward
-    print("Step Info:", "step:", steps, "action:", actions_name[action], "reward:", reward)
-    if terminated or truncated:
-        obs, _ = env.reset()
-        break
-    env.render()
-    time.sleep(0.25)
-    steps += 1
-env.close()
-
-if reward > 0:
-    print("Successful")
-else:
-    print("Fail")
-
-end_time = time.time()  # 记录结束时间
-elapsed_time = end_time - start_time - steps * 0.25  # 计算耗时
-print("步数:", steps, "推测用时:", elapsed_time, "s")
-print("累计奖励:", total_reward)
-
+    test_ppo_model(PPO.load(utils.get_latest_file_concise(out_path)), Env.SokobanEnv(test_map_data), render=True)
