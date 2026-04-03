ad4e60324c510caebf2078746c65e44d93982ad2
None-named <None_named_@outlook.com>
2026-04-03 18:11:16 +0800

    feat:新增检查环境

--- random_test.py
+++ random_test.py
@@ -4,13 +4,15 @@ import utils
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+from stable_baselines3.common.env_checker import check_env
 
 # 定义地图（符号表示）
 test_map_data = utils.parse_map_file("maps/explore/map1.txt")
 
 # 创建环境
 env = Env.SokobanEnv(test_map_data)
-
+# 检查环境
+check_env(env)
 # 运行示例
 obs, _ = env.reset()
 actions_name = {0: "UP", 1: "DOWN", 2: "LEFT", 3: "RIGHT"}
