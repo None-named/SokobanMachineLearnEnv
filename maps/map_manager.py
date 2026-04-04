8aaa07155bb07ff846f40b51fb6f1ecfb271b7e6
None-named <None_named_@outlook.com>
2026-04-04 13:46:41 +0800

    feat:新增获取Map的函数

---
+++ maps/map_manager.py
@@ -0,0 +1,12 @@
+import random
+from pathlib import Path
+import utils
+
+def get_maps(root: Path = Path("./")) -> list[dict[str, str]]:
+    maps: list[dict[str, str]] = []
+    for sub_file in root.iterdir():
+        if sub_file.is_dir():
+            category = sub_file.name
+            for _m in sub_file.iterdir():
+                maps.append({category:str(_m.absolute())})
+    return maps
